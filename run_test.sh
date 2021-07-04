859d66b8c35b496b56b8de8bdcffa63ac61f241d
Line <joceline@gmx.net>
2021-07-04 13:38:37 +0200

    Modify test shell script

---
+++ run_test.sh
@@ -0,0 +1,1 @@
+python test_model.py config.json -d /hpi/fs00/share/fg-arnrich/datasets/chexpert/ -o /scratch/joceline.ziegler/fl_baseline/ -m [] --val
--- run_validation.sh
+++
@@ -1,1 +0,0 @@
-python validate_model.py config.json -d /hpi/fs00/share/fg-arnrich/datasets/chexpert/ -o /scratch/joceline.ziegler/fl_baseline/
