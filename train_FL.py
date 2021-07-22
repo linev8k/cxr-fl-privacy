7915b564b33158a52bcf2f9b1b5620cd5019b5a5
Line <joceline@gmx.net>
2021-07-22 11:21:18 +0200

    Prepare training with new client

--- train_FL.py
+++ train_FL.py
@@ -154,124 +154,124 @@ def main():
     #     print(batch[1][0])
 
 
-    # #create model
-    # if use_gpu:
-    #     model = DenseNet121(nnClassCount, nnIsTrained).cuda()
-    #     # model=torch.nn.DataParallel(model).cuda()
-    # else:
-    #     model = DenseNet121(nnClassCount, nnIsTrained)
-    #
-    # #define path to store results in
-    # output_path = check_path(args.output_path, warn_exists=True)
-    #
-    # fed_start = time.time()
-    # #FEDERATED LEARNING
-    # global_auc = []
-    # best_global_auc = 0
-    # track_no_improv = 0
-    #
-    # for i in range(com_rounds):
-    #
-    #     print(f"[[[ Round {i} Start ]]]")
-    #
-    #     # Step 1: select random fraction of clients
-    #     if fraction < 1:
-    #         sel_clients = sorted(random.sample(clients,
-    #                                        round(num_clients*fraction)))
-    #     else:
-    #         sel_clients = clients
-    #     print("Number of selected clients: ", len(sel_clients))
-    #     print(f"Clients selected: {[sel_cl.name for sel_cl in sel_clients]}")
-    #
-    #     # Step 2: send global model to clients and train locally
-    #     for client_k in sel_clients:
-    #
-    #         # reset model at client's site
-    #         client_k.model_params = None
-    #
-    #         print(f"<< {client_k.name} Training Start >>")
-    #         # set output path for storing models and results
-    #         client_k.output_path = output_path + f"round{i}_{client_k.name}/"
-    #         client_k.output_path = check_path(client_k.output_path, warn_exists=False)
-    #         print(client_k.output_path)
-    #
-    #         train_valid_start = time.time()
-    #         # Step 3: Perform local computations
-    #         # returns local best model
-    #         client_k.model_params = Trainer.train(model, client_k.train_loader, client_k.val_loader,
-    #                                            cfg, client_k.output_path, use_gpu, out_csv=f"round{i}_{client_k.name}.csv")
-    #
-    #         train_valid_end = time.time()
-    #         client_time = round(train_valid_end - train_valid_start)
-    #         print(f"<< {client_k.name} Training Time: {client_time} seconds >>")
-    #
-    #     trained_clients = [cl for cl in clients if cl.model_params != None]
-    #     first_cl = trained_clients[0]
-    #     # last_cl = trained_clients[-1]
-    #     print(f"{[cl.name for cl in trained_clients]}")
-    #
-    #     # Step 4: return updates to server
-    #     for key in first_cl.model_params: #iterate through parameters layerwise
-    #         weights, weightn = [], []
-    #
-    #         for cl in sel_clients:
-    #             weights.append(cl.model_params[key]*len(cl.train_data))
-    #             weightn.append(len(cl.train_data))
-    #         #store parameters with first client for convenience
-    #         first_cl.model_params[key] = sum(weights) / sum(weightn) # weighted averaging model weights
-    #
-    #     if use_gpu:
-    #         model = DenseNet121(nnClassCount).cuda()
-    #         # model = torch.nn.DataParallel(model).cuda()
-    #     # Step 5: server updates global state
-    #     model.load_state_dict(first_cl.model_params)
-    #     # also save intermediate models
-    #     torch.save(model.state_dict(),
-    #                output_path + f"global_{i}rounds.pth.tar")
-    #
-    #     #validate global model on client validation data
-    #     print("Validating global model...")
-    #     aurocMean_global = []
-    #     for cl in clients:
-    #         _, _, cl_aurocMean = Trainer.test(model, cl.val_loader, class_idx, use_gpu, checkpoint=None)
-    #         aurocMean_global.append(cl_aurocMean)
-    #     cur_global_auc = np.array(aurocMean_global).mean()
-    #     print("AUC Mean: {:.3f}".format(cur_global_auc))
-    #     global_auc.append(cur_global_auc)
-    #
-    #     # track early stopping & lr decay
-    #     if cur_global_auc > best_global_auc:
-    #         best_global_auc = cur_global_auc
-    #         track_no_improv = 0
-    #     else:
-    #         track_no_improv += 1
-    #         if track_no_improv == reduce_lr_rounds:
-    #             # decay lr
-    #             cfg['lr'] = cfg['lr'] * 0.1
-    #             print(f"Learning rate reduced to {cfg['lr']}")
-    #         elif track_no_improv == earl_stop_rounds:
-    #             print(f'Global AUC has not improved for {earl_stop_rounds} rounds. Stopping training.')
-    #             break
-    #
-    #     print(f"[[[ Round {i} End ]]]\n")
-    #
-    # # save global AUC in CSV
-    # all_metrics = [list(range(com_rounds)), global_auc]
-    # with open(output_path+'global_validation.csv', 'w') as f:
-    #     header = ['round', 'val AUC']
-    #     writer = csv.writer(f)
-    #     writer.writerow(header)
-    #     writer.writerows(zip(*all_metrics))
-    #
-    # print("Global model trained")
-    # fed_end = time.time()
-    # print(f"Total training time: {round(fed_end-fed_start,0)}")
-    #
-    # # merge local metrics to CSV
-    # try:
-    #     merge_eval_csv(output_path, out_file='train_results.csv')
-    # except:
-    #     print("Merging result CSVs failed. Try again manually.")
+    #create model
+    if use_gpu:
+        model = DenseNet121(nnClassCount, nnIsTrained).cuda()
+        # model=torch.nn.DataParallel(model).cuda()
+    else:
+        model = DenseNet121(nnClassCount, nnIsTrained)
+
+    #define path to store results in
+    output_path = check_path(args.output_path, warn_exists=True)
+
+    fed_start = time.time()
+    #FEDERATED LEARNING
+    global_auc = []
+    best_global_auc = 0
+    track_no_improv = 0
+
+    for i in range(com_rounds):
+
+        print(f"[[[ Round {i} Start ]]]")
+
+        # Step 1: select random fraction of clients
+        if fraction < 1:
+            sel_clients = sorted(random.sample(clients,
+                                           round(num_clients*fraction)))
+        else:
+            sel_clients = clients
+        print("Number of selected clients: ", len(sel_clients))
+        print(f"Clients selected: {[sel_cl.name for sel_cl in sel_clients]}")
+
+        # Step 2: send global model to clients and train locally
+        for client_k in sel_clients:
+
+            # reset model at client's site
+            client_k.model_params = None
+
+            print(f"<< {client_k.name} Training Start >>")
+            # set output path for storing models and results
+            client_k.output_path = output_path + f"round{i}_{client_k.name}/"
+            client_k.output_path = check_path(client_k.output_path, warn_exists=False)
+            print(client_k.output_path)
+
+            train_valid_start = time.time()
+            # Step 3: Perform local computations
+            # returns local best model
+            client_k.model_params = Trainer.train(model, client_k.train_loader, client_k.val_loader,
+                                               cfg, client_k.output_path, use_gpu, out_csv=f"round{i}_{client_k.name}.csv")
+
+            train_valid_end = time.time()
+            client_time = round(train_valid_end - train_valid_start)
+            print(f"<< {client_k.name} Training Time: {client_time} seconds >>")
+
+        trained_clients = [cl for cl in clients if cl.model_params != None]
+        first_cl = trained_clients[0]
+        # last_cl = trained_clients[-1]
+        print(f"{[cl.name for cl in trained_clients]}")
+
+        # Step 4: return updates to server
+        for key in first_cl.model_params: #iterate through parameters layerwise
+            weights, weightn = [], []
+
+            for cl in sel_clients:
+                weights.append(cl.model_params[key]*len(cl.train_data))
+                weightn.append(len(cl.train_data))
+            #store parameters with first client for convenience
+            first_cl.model_params[key] = sum(weights) / sum(weightn) # weighted averaging model weights
+
+        if use_gpu:
+            model = DenseNet121(nnClassCount).cuda()
+            # model = torch.nn.DataParallel(model).cuda()
+        # Step 5: server updates global state
+        model.load_state_dict(first_cl.model_params)
+        # also save intermediate models
+        torch.save(model.state_dict(),
+                   output_path + f"global_{i}rounds.pth.tar")
+
+        #validate global model on client validation data
+        print("Validating global model...")
+        aurocMean_global = []
+        for cl in clients:
+            _, _, cl_aurocMean = Trainer.test(model, cl.val_loader, class_idx, use_gpu, checkpoint=None)
+            aurocMean_global.append(cl_aurocMean)
+        cur_global_auc = np.array(aurocMean_global).mean()
+        print("AUC Mean: {:.3f}".format(cur_global_auc))
+        global_auc.append(cur_global_auc)
+
+        # track early stopping & lr decay
+        if cur_global_auc > best_global_auc:
+            best_global_auc = cur_global_auc
+            track_no_improv = 0
+        else:
+            track_no_improv += 1
+            if track_no_improv == reduce_lr_rounds:
+                # decay lr
+                cfg['lr'] = cfg['lr'] * 0.1
+                print(f"Learning rate reduced to {cfg['lr']}")
+            elif track_no_improv == earl_stop_rounds:
+                print(f'Global AUC has not improved for {earl_stop_rounds} rounds. Stopping training.')
+                break
+
+        print(f"[[[ Round {i} End ]]]\n")
+
+    # save global AUC in CSV
+    all_metrics = [list(range(com_rounds)), global_auc]
+    with open(output_path+'global_validation.csv', 'w') as f:
+        header = ['round', 'val AUC']
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(zip(*all_metrics))
+
+    print("Global model trained")
+    fed_end = time.time()
+    print(f"Total training time: {round(fed_end-fed_start,0)}")
+
+    # merge local metrics to CSV
+    try:
+        merge_eval_csv(output_path, out_file='train_results.csv')
+    except:
+        print("Merging result CSVs failed. Try again manually.")
 
 
 def check_gpu_usage(use_gpu):
