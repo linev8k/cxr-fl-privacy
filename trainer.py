1534666b98fc92fde542085da311341e067f7110
Line <joceline@gmx.net>
2021-11-03 11:32:24 +0100

    Fix conflict in train call

--- trainer.py
+++ trainer.py
@@ -50,11 +50,7 @@ class Trainer():
 
         for epochID in range(0, cfg['max_epochs']):
             train_start.append(time.time()) # training starts
-<<<<<<< HEAD
-            losst = Trainer.epochTrain(model, dataLoaderTrain, optimizer, loss, use_gpu, freeze_mode=freeze_mode)
-=======
             losst = Trainer.epochTrain(client_k.model, client_k.train_loader, client_k.optimizer, loss, use_gpu, freeze_mode=freeze_mode)
->>>>>>> new_opt
             train_end.append(time.time()) # training ends
 
             #validation
